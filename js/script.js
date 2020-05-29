2ea3ad47c628e83281521ab794e4d9e424bd37ef
Fran <franciscosoliscss@gmail.com>
2020-05-28 23:25:59 -0500

    Anclas agregadas al menu y atributos de fullpage en navbar aplicados

--- js/script.js
+++ js/script.js
@@ -1,3 +1,11 @@
 const configureFullPage = new fullpage('#fullpage', {
 
+    // ──────────────────────────────────────────────────
+	//   :::::: Barra de navegación
+	// ──────────────────────────────────────────────────
+    navigation: true, // Muesta la barra de navegación.
+    menu: '#menu', // Menu de navegación.
+    anchors: ['home', 'aboutUs', 'contactUs'], // Anclas, las usamos para identificar cada seccion y poder acceder a ellas con el menu.
+    navigationTooltips: ['Home', 'About Us', 'Contact Us'], // Tooltips que mostrara por cada boton.
+    showActiveTooltip: false, // Mostrar tooltip activa.
 });
