bdc3742bf5140780344e50b8c6492bc9ddd8b7bd
Fran <franciscosoliscss@gmail.com>
2020-05-28 23:59:24 -0500

    Menu agregado al home y con las propiedades de fullpageJS

--- js/script.js
+++ js/script.js
@@ -6,6 +6,6 @@ const configureFullPage = new fullpage('#fullpage', {
     navigation: true, // Muesta la barra de navegación.
     menu: '#menu', // Menu de navegación.
     anchors: ['home', 'aboutUs', 'contactUs'], // Anclas, las usamos para identificar cada seccion y poder acceder a ellas con el menu.
-    navigationTooltips: ['Home', 'About Us', 'Contact Us'], // Tooltips que mostrara por cada boton.
-    showActiveTooltip: false, // Mostrar tooltip activa.
+    navigationTooltips: ['Home', 'About Us', 'Contact Us'], // Tooltips que mostrara por cada boton en el nav.
+    showActiveTooltip: false, // Mostrar tooltip activa si estas en la seccion del tooltip.
 });
