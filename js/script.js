40a17c7b8146484d8de082829469d84ed24c489f
Fran <franciscosoliscss@gmail.com>
2020-05-30 00:58:09 -0500

    Agregando opciones basicas generales de fullpageJS

--- js/script.js
+++ js/script.js
@@ -1,5 +1,16 @@
 const configureFullPage = new fullpage('#fullpage', {
-
+    // ──────────────────────────────────────────────────
+	//   :::::: Opciones Basicas
+	// ──────────────────────────────────────────────────
+		 autoScrolling: true, // Se activa el scroll.
+		 fitToSection: false, // Acomoda el scroll automaticamente para que la seccion se muestre en pantalla.
+		 fitToSectionDelay: 300, // Delay antes de acomodar la seccion automaticamente.
+		 easing: 'easeInOutCubic', // Funcion de tiempo de la animacion.
+		 scrollingSpeed: 600, // Velocidad del scroll. Valores: en milisegundos.
+		 css3: true, // Si usara CSS3 o javascript.
+		 easingcss3: 'ease-out', // Curva de velocidad del efecto.
+         loopBottom: false, // Regresa a la primera seccion siempre y cuando se ya haya llegado a la ultima sección y el ususario siga scrolleando.
+         
     // ──────────────────────────────────────────────────
 	//   :::::: Barra de navegación
 	// ──────────────────────────────────────────────────
@@ -8,4 +19,6 @@ const configureFullPage = new fullpage('#fullpage', {
     anchors: ['home', 'aboutUs', 'contactUs'], // Anclas, las usamos para identificar cada seccion y poder acceder a ellas con el menu.
     navigationTooltips: ['Home', 'About Us', 'Contact Us'], // Tooltips que mostrara por cada boton en el nav.
     showActiveTooltip: false, // Mostrar tooltip activa si estas en la seccion del tooltip.
+
+    verticalCentered: true, // Si alineara de forma vertical los contenidos de cada seccion.
 });
